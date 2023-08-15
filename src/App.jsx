19f154b5d9be23b673fb520df323f0f32588ae37
Munna Ahmed <munnaahmed2025@gmail.com>
2023-08-16 01:43:12 +0600

    add Box4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import "./App.css";
 import Box1 from "./components/Box1";
 import Box2 from "./components/Box2";
 import Box3 from "./components/Box3";
+import Box4 from "./components/Box4";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Box1 />
       <Box2 />
       <Box3 />
+      <Box4 />
     </div>
   );
 }
---
+++ src/components/Box4.jsx
@@ -0,0 +1,22 @@
+import { motion } from "framer-motion";
+
+const Box4 = () => {
+  return (
+    <div>
+      <motion.div
+        className="box hover:cursor-pointer"
+        animate={{
+          scale: [1, 1.4, 0.4, 1, 1],
+          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+          rotate: [0, 0, 270, 270, 0],
+        }}
+        transition={{
+          duration: 2,
+          delay: 0.5,
+        }}
+      />
+    </div>
+  );
+};
+
+export default Box4;
